32178c01574b0bf3210e7eafe2f02e37839b1945
Ranjith jupaka <ranjithjupaka@gmail.com>
2021-07-30 17:48:53 +0530

    Update game.py

--- 4.Tic-tac-toe/game.py
+++ 4.Tic-tac-toe/game.py
@@ -18,7 +18,6 @@ class TicTacToe():
 
     @staticmethod
     def print_board_nums():
-        # 0 | 1 | 2
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
@@ -32,24 +31,22 @@ class TicTacToe():
         return False
 
     def winner(self, square, letter):
-        # check the row
+  
         row_ind = math.floor(square / 3)
         row = self.board[row_ind*3:(row_ind+1)*3]
-        # print('row', row)
+    
         if all([s == letter for s in row]):
             return True
         col_ind = square % 3
         column = [self.board[col_ind+i*3] for i in range(3)]
-        # print('col', column)
+        
         if all([s == letter for s in column]):
             return True
         if square % 2 == 0:
             diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            # print('diag1', diagonal1)
             if all([s == letter for s in diagonal1]):
                 return True
             diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            # print('diag2', diagonal2)
             if all([s == letter for s in diagonal2]):
                 return True
         return False
